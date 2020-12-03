2b84ea18c9399321c5cecb24a9f4f9abffeec931
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-03 11:48:50 -0600

    boilderplate

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,35 +10,36 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import "normalize.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
+      query SiteTitleQuery {
+          site {
+              siteMetadata {
+                  title
+              }
+          }
       }
-    }
   `)
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle={ data.site.siteMetadata?.title || `Title` } />
       <div
-        style={{
+        style={ {
           margin: `0 auto`,
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
+          padding: `0 1.0875rem 1.45rem`
+        } }
       >
-        <main>{children}</main>
-        <footer style={{
+        <main>{ children }</main>
+        <footer style={ {
           marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
+        } }>
+          © { new Date().getFullYear() }, Built with
+          { ` ` }
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
       </div>
@@ -47,7 +48,7 @@ const Layout = ({ children }) => {
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
