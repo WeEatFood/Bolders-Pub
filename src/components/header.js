6a562a4a05acd3125f6cddc42d041fdaf58c6239
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-03 18:20:17 -0600

    Bootstrap setup

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,67 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from "react";
+import * as ReactBootStrap from "react-bootstrap";
+// import { LinkContainer } from "react-router-bootstrap"
+import { Link } from "gatsby";
+import Logo from "../images/bolderlogo.png";
+import "../scss/navbar.scss";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+const Header = () => (
+  <header className="container__navbar">
+    <Link
+      to="/">
+      <img
+        src={ Logo }
+        alt="logo"
+        className="logo" />
+    </Link>
+    <ReactBootStrap.Navbar
+      className="navbar"
+      collapseOnSelect
+      expand="xl"
+      variant="dark">
+      <ReactBootStrap.Navbar.Toggle
+        aria-controls="responsive-navbar-nav" />
+      <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+        <ReactBootStrap.Nav className="mr-auto">
+          <Link className to="/booking">
+            <ReactBootStrap.Nav.Link className="navbar__link"
+                                     style={ { color: "black" } }>
+              BOOK A TABLE
+            </ReactBootStrap.Nav.Link>
+          </Link>
+          <Link to="/online">
+            <ReactBootStrap.Nav.Link className="navbar__link">
+              ORDER ONLINE
+            </ReactBootStrap.Nav.Link>
+          </Link>
+          <Link to="/menu">
+            <ReactBootStrap.Nav.Link className="navbar__link">
+              MENU
+            </ReactBootStrap.Nav.Link>
+          </Link>
+          <Link to="/info">
+            <ReactBootStrap.Nav.Link className="navbar__link">
+              INFO
+            </ReactBootStrap.Nav.Link>
+          </Link>
+        </ReactBootStrap.Nav>
+        <ReactBootStrap.Nav>
+          <Link to="/faq">
+            <ReactBootStrap.Nav.Link className="navbar--right">
+              FAQS
+            </ReactBootStrap.Nav.Link>
+          </Link>
+          <Link to="/contact">
+            <ReactBootStrap.Nav.Link eventKey={ 2 }
+                                     className="navbar--right">
+              CONTACT US
+            </ReactBootStrap.Nav.Link>
+          </Link>
+        </ReactBootStrap.Nav>
+      </ReactBootStrap.Navbar.Collapse>
+    </ReactBootStrap.Navbar>
   </header>
-)
+);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+export default Header;
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,54 +1,26 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from "react";
+import PropTypes from "prop-types";
+import Header from "./header";
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import "normalize.css";
+import "../scss/_config.scss";
+import "./bootstrap.min.css";
+// import "./layout.css";
 
-import Header from "./header"
-import "normalize.css"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-      query SiteTitleQuery {
-          site {
-              siteMetadata {
-                  title
-              }
-          }
-      }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={ data.site.siteMetadata?.title || `Title` } />
-      <div
-        style={ {
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`
-        } }
-      >
-        <main>{ children }</main>
-        <footer style={ {
-          marginTop: `2rem`
-        } }>
-          © { new Date().getFullYear() }, Built with
-          { ` ` }
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    <main>{ children }</main>
+    <footer style={ {
+      marginTop: `2rem`
+    } }>
+      © { new Date().getFullYear() }, Built and designed by Matt and Khai
+    </footer>
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
-}
+};
 
-export default Layout
+export default Layout;
