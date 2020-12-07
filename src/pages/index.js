525c070ce567a2029a8bbc164e61c8cb7c41bde3
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-06 23:26:56 -0600

    styling now

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,26 +10,29 @@ import HomeImg2 from '../images/Food/wine2.jpg';
 import HomeImg3 from '../images/Food/open.jpg';
 import '.././scss/home.scss';
 
-const IndexPage = (props) => (
+const IndexPage = props => (
   <Layout>
-    <SEO title='Home'
-         keywords={ [`gatsby`, `restaurant`, `react`, `group projects`] }/>
-    <Carousel className='carousel__item-container'>
+    <SEO title="Home" keywords={[`gatsby`, `restaurant`, `react`, `group projects`]} />
+    <div className="home-sliderz__container">
+    <Carousel className="carousel__item-container">
       <Carousel.Item>
-        <BackgroundImage fluid={ props.data.slide1.childImageSharp.fluid }
-                         alt='first restaurant image'
-                         className="carousel__image"
+        <BackgroundImage
+          fluid={props.data.slide1.childImageSharp.fluid}
+          alt="first restaurant image"
+          className="carousel__image"
         />
         <Carousel.Caption className="carousel__caption">
           <h3>-Star Magazine</h3>
-          <p>This restaurant is the best in North-West Colorado! Stop by if
-            your in the boulder area</p>
+          <p>
+            This restaurant is the best in North-West Colorado! Stop by if your in the boulder area
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <BackgroundImage fluid={ props.data.slide2.childImageSharp.fluid }
-                         alt='second restaurant image'
-                         className="carousel__image"
+        <BackgroundImage
+          fluid={props.data.slide2.childImageSharp.fluid}
+          alt="second restaurant image"
+          className="carousel__image"
         />
         <Carousel.Caption className="carousel__caption">
           <h3>-Rolling Stones</h3>
@@ -37,77 +40,67 @@ const IndexPage = (props) => (
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <BackgroundImage fluid={ props.data.slide3.childImageSharp.fluid }
-                         alt='third restaurant image'
-                         className="carousel__image"
+        <BackgroundImage
+          fluid={props.data.slide3.childImageSharp.fluid}
+          alt="third restaurant image"
+          className="carousel__image"
         />
         <Carousel.Caption className="carousel__caption">
           <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl
-            consectetur.</p>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <BackgroundImage fluid={ props.data.slide4.childImageSharp.fluid }
-                         alt='fourth restaurant image'
-                         className="carousel__image"
+        <BackgroundImage
+          fluid={props.data.slide4.childImageSharp.fluid}
+          alt="fourth restaurant image"
+          className="carousel__image"
         />
         <Carousel.Caption className="carousel__caption">
           <h3>Fourth slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl
-            consectetur.</p>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    <Info/>
-      <div className="home__image-container">
-        <div className="row">
-          <Col xs={ 6 } md={ 4 }>
-            <Image src={ HomeImg3 } alt="food" fluid/>
-          </Col>
-          <Col xs={ 6 } md={ 4 }>
-            <Image src={ HomeImg1 } alt="food" fluid/>
-          </Col>
-          <Col xs={ 6 } md={ 4 }>
-            <Image src={ HomeImg2 } alt="food" fluid/>
-          </Col>
-        </div>
-      </div>
+    <Info />
+      <img src={HomeImg3} alt="food" className="sliderz" width={20} />
+      <img src={HomeImg1} alt="food" className="sliderz" width={20} />
+      <img src={HomeImg2} alt="food" className="sliderz" width={20}/>
+    </div>
   </Layout>
 );
 
 export default IndexPage;
 
 export const data = graphql`
-	query {
-		slide1: file(relativePath: { eq: "Restaurant/pubhub2.jpg" }) {
-			childImageSharp {
-				fluid {
-					...GatsbyImageSharpFluid_tracedSVG
-				}
-			}
-		}
-		slide2: file(relativePath: { eq: "Restaurant/beer.jpg" }) {
-			childImageSharp {
-				fluid {
-					...GatsbyImageSharpFluid_tracedSVG
-				}
-			}
-		}
-		slide3: file(relativePath: { eq: "Restaurant/rest.jpg" }) {
-			childImageSharp {
-				fluid {
-					...GatsbyImageSharpFluid_tracedSVG
-				}
-			}
-		}
-		slide4: file(relativePath: { eq: "Restaurant/burger.jpg" }) {
-			childImageSharp {
-				fluid {
-					...GatsbyImageSharpFluid_tracedSVG
-				}
-			}
-		}
-	}
+  query {
+    slide1: file(relativePath: { eq: "Restaurant/pubhub2.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    slide2: file(relativePath: { eq: "Restaurant/beer.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    slide3: file(relativePath: { eq: "Restaurant/rest.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    slide4: file(relativePath: { eq: "Restaurant/burger.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
 `;
-
