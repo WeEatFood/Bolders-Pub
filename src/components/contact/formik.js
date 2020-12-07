a8aba8e4b1b6925bcc4806d5aa516f0e47db3805
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-06 20:46:02 -0600

    askj

--- src/components/contact/formik.js
+++ src/components/contact/formik.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Title from '../globals/title';
 import '../../scss/contact.scss';
 
@@ -51,47 +50,6 @@ const Formik = () => (
         </form>
       </div>
     </div>
-=======
-import '../../scss/contact.scss';
-
-const Formik = () => (
-  <section className="formik__section py-5">
-    <title className="formik__title">
-      <div className="row">
-        <div className="col-10 col-sm-8 col-md-6 mx-auto">
-          <form>
-            {/* email */}
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                className="form-control"
-                name="email"
-                placeholder="...Enter a name"
-                id="email"
-              />
-            </div>
-            {/* description */}
-            <div className="formik__form-group">
-              <label htmlFor="description">Description</label>
-              <textarea
-                name="description"
-                id="description"
-                cols=""
-                rows="5"
-                className="formik__text-description"
-                placeholder="Your description here..."
-              />
-            </div>
-            {/* submit */}
-            <button type="submit" className="formik__btn btn btn-block text-capitalize mt-5">
-              Submit
-            </button>
-          </form>
-        </div>
-      </div>
-    </title>
->>>>>>> 20e8c06d1ae7779422e6b792ba22889b1da91cb4
   </section>
 );
 
