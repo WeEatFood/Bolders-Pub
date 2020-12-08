6cbbe33281c30d861cf0d0b7d92db990a315383e
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-08 15:46:10 -0600

    map page together

--- src/pages/info.js
+++ src/pages/info.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Map from '../components/info/map-box';
+import Hours from '../components/info/hours';
 import Layout from '../components/layout';
+import '../scss/info.scss';
 
 function Info() {
   return (
     <Layout>
-      <div>info</div>
+      <div className="info__container">
+        <Map />
+        <Hours />
+      </div>
     </Layout>
   );
 }
