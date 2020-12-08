5c097df15f1c4dbfca17c56a18211bcf4d244c61
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-07 21:53:05 -0600

    menu boilerplate

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Router } from '@reach/router';
+import { Router as MyRouter, Switch, Location } from '@reach/router';
 import { Link } from 'gatsby';
-import { Button } from 'react-bootstrap';
+import { Button, Nav } from 'react-bootstrap';
 import Appetizers from './menu/appetizers';
 import Brewery from './menu/brewery';
 import Cocktails from './menu/cocktails';
@@ -12,54 +12,72 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import '../scss/menu.scss';
 
-const Menu = () => (
-  <Layout>
-    <SEO title="menu" />
-    <div className="menu__container">
-      <h1 className="menu__header">Menus</h1>
-      <p className="menu__header--view">Select a menu to view</p>
-      <div className="menu">
-        <Link to="/menu/appetizers">
-          <Button className="menu__btn" variant="" size="sm">
-            Appetizers
-          </Button>
-        </Link>
-        <Link to="/menu/kitchen">
-          <Button className="menu__btn" variant="" size="sm">
-            Kitchen
-          </Button>
-        </Link>
-        <Link to="/menu/dessert">
-          <Button className="menu__btn" variant="" size="sm">
-            Dessert
-          </Button>
-        </Link>
-        <Link to="/menu/cocktails">
-          <Button className="menu__btn" variant="" size="sm">
-            Cocktails
-          </Button>
-        </Link>
-        <Link to="/menu/brewery">
-          <Button className="menu__btn" variant="" size="sm">
-            Brewery
-          </Button>
-        </Link>
-        <Link to="/menu/wine">
-          <Button className="menu__btn" variant="" size="sm">
-            Wine
-          </Button>
-        </Link>
-        <div className="menu__items">
-          <Router basepath="/menu">
-            <Appetizers path="/appetizers" component={Appetizers} />
-            <Brewery path="/brewery" component={Brewery} />
-            <Cocktails path="/cocktails" component={Cocktails} />
-            <Dessert path="/dessert" component={Dessert} />
-            <Kitchen path="/kitchen" component={Kitchen} />
-            <Wine path="/wine" component={Wine} />
-          </Router>
-        </div>
-      </div>
+
+//
+// const PosedRouter = ({ children }) => (
+//   <Location>
+//     {({ location }) => (
+//         <RouteContainer key={location.key}>
+//           <MyRouter location={location}>{children}</MyRouter>
+//         </RouteContainer>
+//     )}
+//   </Location>
+// );
+
+const Menu = () => {
+
+const MenuNav = () => {
+  <Nav className="menu">
+    <Link to="/menu/appetizers">
+      <Button className="menu__btn" variant="" size="sm">
+        Appetizers
+      </Button>
+    </Link>
+    <Link to="/menu/kitchen">
+      <Button className="menu__btn" variant="" size="sm">
+        Kitchen
+      </Button>
+    </Link>
+    <Link to="/menu/dessert">
+      <Button className="menu__btn" variant="" size="sm">
+        Dessert
+      </Button>
+    </Link>
+    <Link to="/menu/cocktails">
+      <Button className="menu__btn" variant="" size="sm">
+        Cocktails
+      </Button>
+    </Link>
+    <Link to="/menu/brewery">
+      <Button className="menu__btn" variant="" size="sm">
+        Brewery
+      </Button>
+    </Link>
+    <Link to="/menu/wine">
+      <Button className="menu__btn" variant="" size="sm">
+        Wine
+      </Button>
+    </Link>
+  </Nav>
+}
+
+<Layout>
+  <SEO title="menu" />
+  <div className="menu__container">
+    <h1 className="menu__header">Menus</h1>
+    <p className="menu__header--view">Select a menu to view</p>
+        {/*<div className="menu__items">*/}
+          <MyRouter>
+            <MenuNav/>
+            <Switch>
+              <Appetizers path="/menu/appetizers" component={Appetizers} />
+              <Brewery path="/menu/brewery" component={Brewery} />
+              <Cocktails path="/menu/cocktails" component={Cocktails} />
+              <Dessert path="/menu/dessert" component={Dessert} />
+              <Kitchen path="/menu/kitchen" component={Kitchen} />
+              <Wine path="/menu/wine" component={Wine} />
+            </Switch>
+          </MyRouter>
     </div>
   </Layout>
 );
