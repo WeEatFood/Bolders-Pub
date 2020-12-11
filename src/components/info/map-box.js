55c7db905ff64658b8330c6eb437a9279878d021
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-10 19:24:15 -0600

    going in
    Signed-off-by: Matthew Mccarthy <matthewmc96@gmail.com>

--- src/components/info/map-box.js
+++ src/components/info/map-box.js
@@ -1,63 +1,100 @@
 import React, { Component } from 'react';
+import MapGl, { NavigationControl, Marker, Popup } from 'react-map-gl';
 // import Hours from './hours';
-import mapboxgl from 'mapbox-gl';
+import { Icon } from 'semantic-ui-react';
 import '../../scss/info2.scss';
 
-mapboxgl.accessToken =
+const TOKEN =
   'pk.eyJ1IjoibWF0dHlhZGFkYSIsImEiOiJja2hxd2ptYWYwMTE4MnFvNXFkd2NtanlyIn0.6qCzdr06dzoP5Gp5YLPDJQ';
+
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '1em',
+};
+
+const markerList = [
+  {
+    lat: 40.02291,
+    long: -105.26701,
+    name: 'Bolders Pub',
+    info: 10,
+  },
+];
 
 class MapBox extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      lng: -105.26701,
-      lat: 40.02291,
-      zoom: 12,
+      viewport: {
+        latitude: 40.02291,
+        longitude: -105.26701,
+        zoom: 12,
+        bearing: 0,
+        pitch: 0,
+        width: '100%',
+        height: 500,
+      },
+      popupInfo: null,
     };
   }
 
-  componentDidMount() {
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom,
-    });
-
-    // map.doubleClickZoom.enable();
-    // map.boxZoom.enable();
-
-    map.on('move', () => {
-      this.setState({
-        lng: map.getCenter().lng.toFixed(4),
-        lat: map.getCenter().lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2),
-      });
-    });
-    // new mapboxgl.Marker().setLngLat([-105.26701, 40.02291]).addTo(map);
+  renderPopup(index) {
+    return (
+      this.state.popupInfo && (
+        <Popup
+          tipSize={5}
+          anchor="bottom-right"
+          longitude={markerList[index].long}
+          latitude={markerList[index].lat}
+          onMouseLeave={() =>
+            this.setState({
+              popupInfo: null,
+            })
+          }
+          closeOnClick={true}
+        >
+          <p>
+            <strong>{markerList[index].name}</strong>
+            <br />
+            Avaiable beds: {markerList[index].info}
+          </p>
+        </Popup>
+      )
+    );
   }
 
   render() {
+    const { viewport } = this.state;
     return (
-      <div className="map__main-container">
-        {/*<div className="sidebarStyle">*/}
-        {/*</div>*/}
-        {/*<div className="info__divider">*/}
-        {/*  <hr className="solid" />*/}
-        {/*</div>*/}
-        {/*<Hours />*/}
-        <div className="sidebar pad2">Listing</div>
-        {/*<div id="map" className="map pad2">*/}
-        {/*  Map*/}
-        {/*</div>*/}
-        <div className="map__container">
-          <div className="map__sidebar pad2">
-            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}
-          </div>
-          <div ref={el => (this.mapContainer = el)} id="map" className="map pad2" />
+      <MapGl
+        {...viewport}
+        onViewportChange={viewport => this.setState({ viewport })}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxApiAccessToken={TOKEN}
+      >
+        <div className="nav" style={navStyle}>
+          <NavigationControl onViewportChange={viewport => this.setState({ viewport })} />
+          {markerList.map((marker, index) => {
+            return (
+              <div key={index}>
+                {' '}
+                <Marker longitude={marker.long} latitude={marker.lat}>
+                  <Icon
+                    name="hospital"
+                    size="big"
+                    onMouseEnter={() => this.setState({ popupInfo: true })}
+                    onMouseLeave={() => this.setState({ popupInfo: null })}
+                  />
+                </Marker>{' '}
+                {this.renderPopup(index)}
+              </div>
+            );
+          })}
         </div>
-      </div>
+      </MapGl>
     );
   }
 }
