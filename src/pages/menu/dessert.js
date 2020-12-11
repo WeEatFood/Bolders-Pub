5bcb07a452c2e1bd20a74d13d300e2d3f959b75b
Thang Khai <tnkhai94@gmail.com>
2020-12-10 17:06:11 -0700

    dessert restructure complete

--- src/pages/menu/dessert.js
+++ src/pages/menu/dessert.js
@@ -1,68 +1,77 @@
 import React from 'react';
 import '../../scss/sub-menu.scss';
+import '../../scss/dessert.scss';
 import PropTypes from 'prop-types';
 import Layout from '../../components/layout';
 
 const Dessert = props => {
   return (
     <Layout>
-    <div className="sub__container">
-      <h4 className="sub__header">Dessert</h4>
-      <div className="menu-item">
-        <h4 className="menu-item__header">DARK CHOCOLATE CAKE</h4>
-        <p className="menu-item__details--description">
-          Decadent dark chocolate cake baked with a touch of cinnamon
-        </p>
-        <p className="menu-item__details--price">8</p>
+      <h4 className="dessert__header">Dessert</h4>
+
+      <div className="dessert-item">
+        <h4 className="dessert-item__header">DARK CHOCOLATE CAKE</h4>
+        <section className="dessert-item__details">
+          <p className="menu-item__details--description">
+            Decadent dark chocolate cake baked with a touch of cinnamon</p>
+          <p className="menu-item__details--price">8</p>
+        </section>
       </div>
 
-      <div className="menu-item">
-        <h4 className="menu-item__header">CHEESECAKE</h4>
-        <p className="menu-item__details--description">
-          Traditional New York style cheesecake with berry sauce
-        </p>
-        <p className="menu-item__details--price">6</p>
+      <div className="dessert-item">
+        <h4 className="dessert-item__header">CHEESECAKE</h4>
+        <section className="dessert-item__details">
+          <p className="menu-item__details--description">
+            Traditional New York style cheesecake with berry sauce</p>
+          <p className="menu-item__details--price">6</p>
+        </section>
       </div>
 
-      <div className="menu-item">
-        <h4 className="menu-item__header">CARAMEL BREAD PUDDING</h4>
-        <p className="menu-item__details--description">
-          House-made seasonal bread pudding with caramel sauce.Ask your server for today's selection
-        </p>
-        <p className="menu-item__details--price">6</p>
+      <div className="dessert-item">
+        <h4 className="dessert-item__header">CARAMEL BREAD PUDDING</h4>
+        <section className="dessert-item__details">
+          <p className="menu-item__details--description">
+            House-made seasonal bread pudding with caramel sauce.Ask your server for today's selection</p>
+          <p className="menu-item__details--price">6</p>
+        </section>
       </div>
 
-      <div className="menu-item">
-        <h4 className="menu-item__header">TIRAMISU</h4>
-        <p className="menu-item__details--description">
-          Lady fingers flavored with espresso, layered with sweetened Italian cream cheese
-        </p>
-        <p className="menu-item__details--price">8</p>
+      <div className="dessert-item">
+        <h4 className="dessert-item__header">TIRAMISU</h4>
+        <section className="dessert-item__details">
+          <p className="menu-item__details--description">
+            Lady fingers flavored with espresso, layered with sweetened Italian cream cheese</p>
+          <p className="menu-item__details--price">8</p>
+        </section>
       </div>
 
-      <div className="menu-item">
-        <h4 className="menu-item__header">CHOCOLATE HAZELNUT MOUSSE CAKE</h4>
-        <p className="menu-item__details--description">
-          Chocolate hazelnut mousse layered between a shortbread crust and dark chocolate ganache
-          topping
-        </p>
-        <p className="menu-item__details--price">8</p>
+      <div className="dessert-item">
+        <h4 className="dessert-item__header">CHOCOLATE HAZELNUT MOUSSE CAKE</h4>
+        <section className="dessert-item__details">
+          <p className="menu-item__details--description">
+            Chocolate hazelnut mousse layered between a shortbread crust and dark chocolate ganache
+          topping</p>
+          <p className="menu-item__details--price">8</p>
+        </section>
       </div>
 
-      <div className="menu-item">
-        <h4 className="menu-item__header">CREME BRULEE</h4>
-        <p className="menu-item__details--description">
-          Traditional creamy vanilla bean custard with a caramelized sugar topping
-        </p>
-        <p className="menu-item__details--price">11</p>
+      <div className="dessert-item">
+        <h4 className="dessert-item__header">CREME BRULEE</h4>
+        <section className="dessert-item__details">
+          <p className="menu-item__details--description">
+            Traditional creamy vanilla bean custard with a caramelized sugar topping</p>
+          <p className="menu-item__details--price">11</p>
+        </section>
       </div>
 
-      <div className="menu-item">
-        <h4 className="menu-item__header">GELATO & SORBET</h4>
-        <p className="menu-item__details--description">Ask your server for today's selections</p>
-        <p className="menu-item__details--price">7</p>
+      <div className="dessert-item">
+        <h4 className="dessert-item__header">GELATO & SORBET</h4>
+        <section className="dessert-item__details">
+          <p className="menu-item__details--description">Ask your server for today's selections</p>
+          <p className="menu-item__details--price">7</p>
+        </section>
       </div>
-    </div>
+
     </Layout>
   );
 };
