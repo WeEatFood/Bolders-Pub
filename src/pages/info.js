8854ff1da985cfc1aa189b58a9d296fb1546fbe8
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-14 18:43:06 -0600

    chores: every page pretty much
    Signed-off-by: Matthew Mccarthy <matthewmc96@gmail.com>

--- src/pages/info.js
+++ src/pages/info.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import Map from '../components/info/map';
+import Index from '../components/info/index';
 import Layout from '../components/layout';
-import Hours from '../components/info/hours';
-import Subscribe from '../components/contact/subscribe';
 import '../scss/info.scss';
 import Spec from '../images/Nia-Sign-With-Weekly-Specials-1024x524.jpg';
 // import Subscribe from '../components/contact/subscribe';
@@ -11,7 +9,7 @@ const Info = () => {
   return (
     <Layout>
       <div className="info__container">
-        <Map />
+        <Index />
       </div>
     </Layout>
   );
